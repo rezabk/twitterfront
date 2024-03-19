a2d374e7a426304e672fa3d9762bf0b5aa746139
earncoin4ever <earncoin4ever@gmail.com>
2024-03-19 09:11:15 +0330

    package added4

--- src/api/AdminServices.js
+++ src/api/AdminServices.js
@@ -4,3 +4,7 @@ import config from "./config.json";
 export const loginAdmin = (user) => {
   return http.post(`${config.ecoinapi}auth/loginAdmin`, user);
 };
+
+export const showUsers = () => {
+  return http.get(`${config.ecoinapi}auth/admin/getusers`);
+};
